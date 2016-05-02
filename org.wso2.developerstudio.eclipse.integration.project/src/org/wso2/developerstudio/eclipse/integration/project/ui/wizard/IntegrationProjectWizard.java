aa70af67a16b41b2b68f9a55f9d39853ee23cc44
Maheeka Jayasuriya <maheeka@wso2.com>
2016-05-02 15:43:51 +0530

    create integration type project with folder structure

--- org.wso2.developerstudio.eclipse.integration.project/src/org/wso2/developerstudio/eclipse/integration/project/ui/wizard/IntegrationProjectWizard.java
+++ org.wso2.developerstudio.eclipse.integration.project/src/org/wso2/developerstudio/eclipse/integration/project/ui/wizard/IntegrationProjectWizard.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.axiom.om.OMElement;
 //import org.apache.axiom.om.OMElement;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
@@ -16,9 +17,12 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
+import org.wso2.developerstudio.eclipse.integration.core.utils.IntegrationUtils;
 //import org.wso2.developerstudio.eclipse.esb.core.utils.SynapseUtils;
 import org.wso2.developerstudio.eclipse.integration.project.Activator;
+import org.wso2.developerstudio.eclipse.integration.project.artifact.IntegrationProjectArtifact;
 import org.wso2.developerstudio.eclipse.integration.project.model.IntegrationProjectModel;
+import org.wso2.developerstudio.eclipse.integration.utils.IntegrationProjectUtils;
 //import org.wso2.developerstudio.eclipse.esb.project.artifact.ESBProjectArtifact;
 //import org.wso2.developerstudio.eclipse.esb.project.model.ESBProjectModel;
 //import org.wso2.developerstudio.eclipse.esb.project.utils.ESBImageUtils;
@@ -34,7 +38,7 @@ import org.wso2.developerstudio.eclipse.utils.project.ProjectUtils;
 
 public class IntegrationProjectWizard extends AbstractWSO2ProjectCreationWizard {
 	private static IDeveloperStudioLog log=Logger.getLog(Activator.PLUGIN_ID);
-	private static final String ESB_PROJECT_NATURE = "org.wso2.developerstudio.eclipse.esb.project.nature";
+	private static final String INTEGRATION_PROJECT_NATURE = "org.wso2.developerstudio.eclipse.integration.project.nature";//TODO integration prokect nature
 	private IProject project;
 	private IntegrationProjectModel integrationProjectModel;
 	private File pomfile;
@@ -48,82 +52,74 @@ public class IntegrationProjectWizard extends AbstractWSO2ProjectCreationWizard 
 	
 	public boolean performFinish() {
 		System.out.println("Perform finish....");
-//		try {
-//			List<OMElement> esbArtiList = null; 
-//			if (esbProjectModel.getSelectedOption().equals("new.esb.synapseConfig")) {
-//				esbArtiList =SynapseUtils.synapseConfigFolderContentProcessing(esbProjectModel.getSynapseConfigLocation().getPath());
-//			}
-//			project = createNewProject();
-//			pomfile = project.getFile("pom.xml").getLocation().toFile();
-//			createPOM(pomfile,"pom");
-//			ProjectUtils.addNatureToProject(project,
-//											false,
-//			                                ESB_PROJECT_NATURE);
-//			MavenUtils
-//			.updateWithMavenEclipsePlugin(
-//					pomfile,
-//					new String[] { },
-//					new String[] { ESB_PROJECT_NATURE });
-//			
-//			//Creating the metadata file artifact.xml while creating the ESB project. It will be hidden and users won't be able to see it via Eclipse.
-//			ESBProjectArtifact artifact=new ESBProjectArtifact();
-//			IFile file = project.getFile("artifact.xml");
-//			artifact.setSource(file.getLocation().toFile());
-//			artifact.toFile();
-//			getModel().addToWorkingSet(project);
-//			//Refresh the project to show the changes. But still won't see the newly created project.
-//			project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
-//			refreshDistProjects();
-//			//Setting the created file to be hidden so that users won't see it.
-//			if(file.exists()){
-//				file.setHidden(true);
-//			}
-//			String groupId = getMavenGroupId(pomfile);
-//			if (esbProjectModel.getSelectedOption().equals("new.esb.synapseConfig")) {				
-//				ESBProjectUtils.createESBArtifacts(esbArtiList,project,pomfile,fileList,groupId);
-//				
-//				ESBProjectUtils.updatePom(project);
-//				project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
-//				refreshDistProjects();
-//				if (!fileList.isEmpty()) {
-//		            if (MessageDialog.openQuestion(getShell(), "Open file(s) in the Editor",
-//		                                           "Do you like to open the file(s) in Developer Studio?")) {
-//			            for (File f : fileList.keySet()) {
-//			            	openEditor(f,fileList);
-//			            }
-//		            }
-//	            }
-//			}
-//		}catch (Exception e) {
-//			MessageDialog.openError(getShell(), "Error while creating the project",
-//                    e.getMessage());
-//			return false;
-//
-//		}
+		try {
+			List<OMElement> integrationArtifactList = null; 
+			if (integrationProjectModel.getSelectedOption().equals("new.integration.integrationConfig")) {
+				integrationArtifactList =IntegrationUtils.integrationConfigFolderContentProcessing
+						(integrationProjectModel.getIntegrationConfigLocation().getPath());
+			}
+			project = createNewProject();
+			pomfile = project.getFile("pom.xml").getLocation().toFile();
+			createPOM(pomfile,"pom");
+			ProjectUtils.addNatureToProject(project, false, INTEGRATION_PROJECT_NATURE);
+			MavenUtils.updateWithMavenEclipsePlugin(pomfile,new String[] { },new String[] { INTEGRATION_PROJECT_NATURE });
+			
+			//Creating the metadata file artifact.xml while creating the ESB project. It will be hidden and users won't be able to see it via Eclipse.
+			IntegrationProjectArtifact artifact=new IntegrationProjectArtifact();
+			IFile file = project.getFile("artifact.xml");
+			artifact.setSource(file.getLocation().toFile());
+			artifact.toFile();
+			getModel().addToWorkingSet(project);
+			//Refresh the project to show the changes. But still won't see the newly created project.
+			project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+			refreshDistProjects();
+			//Setting the created file to be hidden so that users won't see it.
+			if(file.exists()){
+				file.setHidden(true);
+			}
+			String groupId = getMavenGroupId(pomfile);
+			if (integrationProjectModel.getSelectedOption().equals("new.integration.integrationConfig")) {				
+				IntegrationProjectUtils.createIntegrationArtifacts(integrationArtifactList,project,pomfile,fileList,groupId);
+				
+				IntegrationProjectUtils.updatePom(project);
+				project.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
+				refreshDistProjects();
+				if (!fileList.isEmpty()) {
+		            if (MessageDialog.openQuestion(getShell(), "Open file(s) in the Editor",
+		                                           "Do you like to open the file(s) in Developer Studio?")) {
+			            for (File f : fileList.keySet()) {
+			            	openEditor(f,fileList);
+			            }
+		            }
+	            }
+			}
+		}catch (Exception e) {
+			MessageDialog.openError(getShell(), "Error while creating the project",
+                    e.getMessage());
+			return false;
+
+		}
 		return true;
 	}
 	
-//	public static void openEditor(File file,Map<File,String> fileList){
-//		try {
-//			String type = fileList.get(file);
-//			IFile dbsFile  = ResourcesPlugin
-//			.getWorkspace()
-//			.getRoot()
-//			.getFileForLocation(
-//					Path.fromOSString(file.getAbsolutePath()));
-//			if("synConfig".equals(type)){
-//				IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(),dbsFile);
-//			}else{
-//				String path = dbsFile.getParent().getFullPath()+"/";
-//				String source = FileUtils.getContentAsString(file);
-//				Openable openable = ESBGraphicalEditor.getOpenable();
-//				openable.editorOpen(file.getName(),type,path+type+"_", source);
-//			}
-//
-//		} catch (Exception e) { 
-//			log.error("cannot open editor",e);
-//		}
-//	}
+	public static void openEditor(File file,Map<File,String> fileList){
+		try {
+			String type = fileList.get(file);
+			IFile dbsFile  = ResourcesPlugin.getWorkspace().getRoot()
+					.getFileForLocation(Path.fromOSString(file.getAbsolutePath()));
+			if("integrationConfig".equals(type)){
+				IDE.openEditor(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage(),dbsFile);
+			}else{
+				String path = dbsFile.getParent().getFullPath()+"/";
+				String source = FileUtils.getContentAsString(file);
+				Openable openable = ESBGraphicalEditor.getOpenable();
+				openable.editorOpen(file.getName(),type,path+type+"_", source);
+			}
+
+		} catch (Exception e) { 
+			log.error("cannot open editor",e);
+		}
+	}
 
 	public IntegrationProjectModel getIntegrationProjectModel() {
 		return integrationProjectModel;
